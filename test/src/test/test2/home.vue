137d973d711341d04c9fbe4078f71087862d6638
Klaas Van Parys <klaas@ivox.be>
2023-12-09 17:11:01 +0100

    Added 2 test files

---
+++ test/src/test/test2/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="home">
+		<h1>Hello world</h1>
+		<custom-input></custom-input>
+	</div>
+</template>
+
+<script>
+import Input from "./input.vue";
+
+const test = true;
+
+export default {
+	name: "home",
+	components: {
+		"custom-input": Input
+	}
+}
+</script>
+
+<style scoped>
+#home h1 { font-size: 1.5rem; }
+</style>
---
+++ test/src/test/test2/input.vue
@@ -0,0 +1,15 @@
+<template>
+    <label>
+		  <input type="number" v-model="number" />
+    </label>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const number = ref(10);
+</script>
+
+<style scoped>
+input[type="number"] { font-size: 0.8rem; }
+</style>
